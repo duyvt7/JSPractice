9df9846462742302045ffdac4042a43b3367bbee
duyvt <vuthanhduy93@gmail.com>
2021-08-19 21:01:17 +0700

    prototype method

--- class.js
+++ class.js
@@ -6,17 +6,31 @@
 // }
 // console.log(rectangle.name)
 // class expression
-let rectangle = class{
+// let rectangle = class{
+//     constructor(width, length){
+//         this.width = width;
+//         this.length = length;
+//     }
+// }
+// console.log(rectangle.name)
+// let Rectangle = class rectangle2 {
+//     constructor(width, length){
+//         this.width=width;
+//         this.length = length;
+//     }
+// }
+// console.log(Rectangle.name)
+
+// prototype method
+
+class rectangle {
     constructor(width, length){
         this.width = width;
         this.length = length;
     }
-}
-console.log(rectangle.name)
-let Rectangle = class rectangle2 {
-    constructor(width, length){
-        this.width=width;
-        this.length = length;
+    calArea (){
+        return this.width* this.length;
     }
 }
-console.log(Rectangle.name)
+const square = new rectangle(10,10)
+console.log(square.calArea())
