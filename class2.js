d0c4b8a224779ab0993221c1627376e3142bcb4b
duyvt <vuthanhduy93@gmail.com>
2021-08-25 21:27:31 +0700

    some note with static

--- class2.js
+++ class2.js
@@ -14,3 +14,21 @@ var x = new rectangle(10,5)
 console.log(x.getArea())
 console.log(rectangle.getParemeter(10,10))
 
+// binding this with prototype and static method 
+
+class animal{
+    constructor(type){
+        this.type = type
+    }
+    static eat(){
+        return console.log(`${this.type} is eatting`)
+    }
+    eat2(){
+        return console.log(`${this.type} is eatting`)
+    }
+}
+animal.eat('dog')
+let z = new animal('dog')
+console.log(z.eat2())
+
+
