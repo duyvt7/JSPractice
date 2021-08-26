06267b4deca9d3f85cf6cf3510482000f59a8bdb
duyvt <vuthanhduy93@gmail.com>
2021-08-26 21:47:15 +0700

    react practice

---
+++ test/src/component/TodoItem.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import './TodoItem.css'
+
+
+class TodoItem extends Component{
+    
+    render(){
+        return(
+            <div className= 'App'> 
+                <p> {this.props.item.title} </p>
+            </div>
+        )
+    }
+}
+export default TodoItem
